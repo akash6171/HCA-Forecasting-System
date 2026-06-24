2fd0041fe36f186aca21b460346739addc0131c2
akash6171 <111282871+akash6171@users.noreply.github.com>
2026-06-24 12:00:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,25 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Demand Forecasting & Inventory Intelligence System")
+st.title("Demand Forecasting & Inventory Intelligence System:-")
 
+st.info("""
+Welcome to Hari Chand Anand & Co.'s Demand Forecasting &
+Inventory Intelligence Platform.
+
+This system helps forecast future demand,
+monitor inventory levels, and generate
+restocking recommendations.
+""")
 st.markdown("""
-### Features
+### Features:-
 
 - Dashboard
 - EDA Analysis
 - Demand Forecasting
 - Restocking Engine
 - Model Performance
+- Batch Prediction
 
 Use the sidebar to navigate.
 """)
